7504e6815a17070e1537d2f4bead2e71e054b100
Maximilian Torggler <maximilian.torggler@tuta.io>
2019-04-03 17:26:44 +0200

    Added Config template.

---
+++ src/config.template.js
@@ -0,0 +1,16 @@
+module.exports = {
+  ampacheApi: {
+    username: "ampache_key_name",
+    password: "ampache_key",
+    url:
+      "https://<server_url>/index.php/apps/music/ampache/server/xml.server.php"
+  },
+  musicServer: {
+    url: "https://<server_url>/index.php/apps/music/api"
+  },
+  nextCloud: {
+    username: "nextcloud_username",
+    password: "nextcloud_password"
+  },
+  musicFolder: "~/Music"
+};
